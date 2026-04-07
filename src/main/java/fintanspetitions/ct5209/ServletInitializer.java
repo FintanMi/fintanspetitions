be00cd8e9eadd8d24838c8fb930c952b32e64f79
fino <fintanjnr@gmail.com>
2026-04-07 21:30:13 +0100

    Added ServletInitializer file for the tomcat server

---
+++ src/main/java/fintanspetitions/ct5209/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fintanspetitions.ct5209;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Ct5209Application.class);
+    }
+
+}
